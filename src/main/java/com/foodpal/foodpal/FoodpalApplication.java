ede194fa863b9e2f22651a03a587cc5a1bbd7b58
Michael Smith <mdsf3f@mail.missouri.edu>
2017-10-08 22:22:23 -0500

    Initial Spring Project setup

---
+++ src/main/java/com/foodpal/foodpal/FoodpalApplication.java
@@ -0,0 +1,12 @@
+package com.foodpal.foodpal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodpalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodpalApplication.class, args);
+	}
+}
